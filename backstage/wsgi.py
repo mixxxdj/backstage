f187ebe9d7252ff5db33baa431b49c89c3f270eb
weixindut <weixindlut@gmail.com>
2013-06-20 22:53:01 +0800

    WSGI config for backstage project

--- backstage/wsgi.py
+++ backstage/wsgi.py
@@ -19,14 +19,10 @@ import os
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "backstage.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backstage.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backstage.settings_production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
