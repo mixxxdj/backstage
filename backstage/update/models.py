e320315c943fbaa89f6154ce85511f82df42c7fb
weixindut <weixindlut@gmail.com>
2013-06-20 23:29:11 +0800

    update

---
+++ backstage/update/models.py
@@ -0,0 +1,11 @@
+# coding: UTF-8
+'''
+Created: 2013-06-20
+Author: weixin
+Email: weixindlut@gmail.com
+Desc:
+
+'''
+from django.db import models
+
+# Create your models here.
---
+++ backstage/update/views.py
@@ -0,0 +1,8 @@
+'''
+Created: 2013-06-20
+Author: weixin
+Email: weixindlut@gmail.com
+Desc:
+
+'''
+# Create your views here.
