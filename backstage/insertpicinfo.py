26ad2396c271cd0cd03faf943887d6f7d1e922e5
weixindut <weixindlut@gmail.com>
2013-07-23 16:17:32 +0800

    script used for inserting picture info into preset files

--- backstage/insertpicinfo.py
+++ backstage/insertpicinfo.py
@@ -1,13 +1,16 @@
 #coding: UTF-8
 
 import os
-import sys
 from xml.etree import ElementTree as ET
 
 
 #root name defination
+TREE_INFO = "info"
 TREE_CONTROLLER = "controller"
+
+PRESET_NAME = "name"
 PICFILES = "picfiles"
+PIC = "pic"
 
 
 def insert_pic_info(path=None):
@@ -18,26 +21,46 @@ def insert_pic_info(path=None):
     """
     if path is None:
         print "input args is error"
-        return None
+        return
 
     if not os.path.exists(path):
         print "Sorry, our system cannot find this path: %s" % (path)
-        return None
+        return
 
     tree = ET.parse(path)
     root = tree.getroot()
     controller_tag = root.find(TREE_CONTROLLER)
     pic_tag = ET.Element(PICFILES)
-    #ET.SubElement(controller_tag, PICFILES)
-    pic_tag.text = "pic Test"
+    pic = ET.SubElement(pic_tag, PIC)
+    pic_name = get_pic_name(path)
+    pic.set('name', pic_name)
     controller_tag.insert(1, pic_tag)
     tree.write(path)
     return
 
 
-def test_insert():
-    path = "/home/amaris/dev-mixxx/backstage/backstage/test/controllers/Akai-LPD8-RK.midi.xml"
-    insert_pic_info(path)
+def get_pic_name(path=None):
+    """
+    get picture name, same with preset name
+    """
+    if path is None:
+        print "input args is error"
+        return None
+    if not os.path.exists(path):
+        print "Sorry, our system cannot find this path: %s" % (path)
+        return None
+    pic_jpg = path.replace('.midi.xml', '.jpg')
+    pic_png = path.replace('.midi.xml', '.png')
+    pic_gif = path.replace('.midi.xml', '.gif')
+    if os.path.exists(pic_jpg):
+        return os.path.basename(pic_jpg)
+    elif os.path.exists(pic_png):
+        return os.path.basename(pic_png)
+    elif os.path.exists(pic_gif):
+        return os.path.basename(pic_gif)
+    else:
+        print "no pic files for this preset file%s" % os.path.basename(path)
+        return ""
 
 
 def get_specific_files(path=None):
@@ -79,14 +102,5 @@ def midi_insert(path=None):
             ret[f] = insert_pic_info(f)
 
 
-def test_get_path():
-    path = "/Users/amaris/dev/xmlparse/"
-    get_specific_files(path)
-
-def test():
-    test_insert()
-    test_get_path()
-
 if __name__ == "__main__":
-   # midi_insert(path="/home/amaris/dev-mixxx/backstage/backstage/xmlparse/controllers")
-    test_insert()
+    midi_insert(path="/home/amaris/dev-mixxx/backstage/backstage/test/controllers")
