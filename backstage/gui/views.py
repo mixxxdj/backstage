72ebcac00648dd7339924c767ef9b7586287a3cb
weixindut <weixindlut@gmail.com>
2013-08-05 23:32:51 +0800

    accept file upload

--- backstage/gui/views.py
+++ backstage/gui/views.py
@@ -5,6 +5,9 @@ Email: weixindlut@gmail.com
 Desc:
 
 '''
+import uuid
+import os
+from django.conf import settings
 from backstage.backend.logging import loginfo
 from django.utils import simplejson
 from django.http import HttpResponse
@@ -13,21 +16,15 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def upload(request):
-    loginfo(p="--" * 10)
-    loginfo(p=request)
-    loginfo(p="--" * 10)
     if request.method == "POST":
-        loginfo(p="**" * 10)
-        loginfo(p=request.FILES)
-        f = request.FILES["file"]
-        with open("/home/amaris/dev/text.cpp", "wb+") as dest:
-            for chunk in f.chunks():
-                dest.write(chunk)
+        f = request.raw_post_data.split("&", 1)
+        loginfo(f[0])
+        path = os.path.join(settings.TMP_FILE_PATH, f[0])
+        with open(path, "wb+") as dest:
+            dest.write(f[1])
         data = simplejson.dumps({"status": True})
     else:
         loginfo(p="get")
         data = simplejson.dumps({"status": False})
-
-
 
     return HttpResponse(data, mimetype='application/json')
